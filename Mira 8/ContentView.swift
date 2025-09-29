e1aa9d1a0f573a9f5ae4172c7b6bc331fe740cc4
Lauren Barnhart <barnhartl91@gmail.com>
2025-09-29 17:00:20 -0400

    Initial Commit

---
+++ Mira 8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mira 8
+//
+//  Created by Lauren Barnhart on 9/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mira 8/Mira_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Mira_8App.swift
+//  Mira 8
+//
+//  Created by Lauren Barnhart on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Mira_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
